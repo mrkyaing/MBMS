67c618b192fb14c87297c9fdf49419e7d3999543
Mg Kyaing <akog02@gmaill.com>
2019-12-09 16:12:52 +0630

    meter bill calculate (Controller ,service ,UI)

---
+++ MPS.BusinessLogic/MeterBillCalculationController/IMeterBillCalculateServices.cs
@@ -0,0 +1,12 @@
+﻿using MBMS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPS.BusinessLogic.MeterBillCalculationController {
+    interface IMeterBillCalculateServices {
+        void MeterBillCalculate(List<MeterBill> _meterBill);
+        }
+    }
---
+++ MPS.BusinessLogic/MeterBillCalculationController/MeterBillCalculateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MBMS.DAL;
+
+namespace MPS.BusinessLogic.MeterBillCalculationController {
+    public class MeterBillCalculateController : IMeterBillCalculateServices {
+        MBMSEntities mBMSEntities = new MBMSEntities();
+        public void MeterBillCalculate(List<MeterBill> meterBillList) {
+           foreach(MeterBill item in meterBillList) {
+                mBMSEntities.MeterBills.Add(item);
+                mBMSEntities.SaveChanges();
+                }
+            }
+        }
+    }
