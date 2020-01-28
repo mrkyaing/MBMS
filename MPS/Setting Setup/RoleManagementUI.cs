c07dcc483a2e72b65bb3f7e6c61bf99f97c17823
Mg Kyaing <akog02@gmaill.com>
2020-01-28 10:37:43 +0630

    role mgt complete code

--- MPS/Setting Setup/RoleManagementUI.cs
+++ MPS/Setting Setup/RoleManagementUI.cs
@@ -52,7 +52,11 @@ namespace MPS.Setting_Setup {
                     roleController.SaveRoleMgtByRoleID("BillProcess", "Edit", roleID, chkbillingprocess.Checked, UserID);
                     roleController.SaveRoleMgtByRoleID("BillProcess", "View", roleID, chkviewbillinginvoice.Checked, UserID);
                     roleController.SaveRoleMgtByRoleID("BillProcess", "Edit", roleID, chkeditbillingprocessdata.Checked, UserID);
-                    //
+                    //Bill Code  Layer
+                    roleController.SaveRoleMgtByRoleID("BillCodeLayer", "Add", roleID, chkaddbillcodelayer.Checked, UserID);
+                    roleController.SaveRoleMgtByRoleID("BillCodeLayer", "View", roleID, chkviewbillcodelayerlist.Checked, UserID);
+                    roleController.SaveRoleMgtByRoleID("BillCodeLayer", "Edit", roleID, chkeditdeletebillcodelayer.Checked, UserID);
+                    //Meter
                     roleController.SaveRoleMgtByRoleID("Meter", "Edit", roleID, chkeditdeletemeter.Checked, UserID);
                     roleController.SaveRoleMgtByRoleID("Meter", "Add", roleID, chkaddmeter.Checked, UserID);
                     roleController.SaveRoleMgtByRoleID("Meter", "View", roleID, chkviewmeterlist.Checked, UserID);
@@ -84,18 +88,70 @@ namespace MPS.Setting_Setup {
             }
 
         private void btnCancel_Click(object sender, EventArgs e) {
-            chkaddcustomer.Checked = chkeditdeletecustomer.Checked = chkviewcustomerlist.Checked = false;
+            resetCheckControl(false);
             cboUserRole.SelectedIndex = 0;
             }
 
         private void cboUserRole_SelectedIndexChanged(object sender, EventArgs e) {
             string roleID = cboUserRole.SelectedValue.ToString();
+            resetCheckControl(false);
             List<RoleManagement> rolemgtList = mbmsEntities.RoleManagements.Where(x => x.Active == true && x.RoleID == roleID).ToList();
             foreach(RoleManagement item in rolemgtList) {
+                //customer 
                 if (item.RoleFeatureName.Equals("CustomerView")) chkviewcustomerlist.Checked = item.IsAllowed;
                 if (item.RoleFeatureName.Equals("CustomerAdd")) chkaddcustomer.Checked = item.IsAllowed;
                 if (item.RoleFeatureName.Equals("CustomerEditOrDelete")) chkeditdeletecustomer.Checked = item.IsAllowed;
+                //bill pay ment 
+                if (item.RoleFeatureName.Equals("BillPaymentView")) chkviewbillpayment.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("BillPaymentEditOrDelete")) chkeditdeletebillpayment.Checked = item.IsAllowed;
+
+                //bill process
+                if (item.RoleFeatureName.Equals("BillProcessEditOrDelete")) chkbillingprocess.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("BillProcessView")) chkviewbillinginvoice.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("CustomerEditOrDelete")) chkeditbillingprocessdata.Checked = item.IsAllowed;
+
+                //bill unit 
+                if (item.RoleFeatureName.Equals("BillUnitCollectView")) chkViewBillUnit.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("BillUnitCollectEditOrDelete")) chkcollectbillunit.Checked = item.IsAllowed;
+
+                //Meter 
+                if (item.RoleFeatureName.Equals("MeterEditOrDelete")) chkeditdeletemeter.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("MeterAdd")) chkaddmeter.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("MeterView")) chkviewmeterlist.Checked = item.IsAllowed;
+                //Role
+                if (item.RoleFeatureName.Equals("RoleEditOrDelete")) chkeditdeleterole.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("RoleAdd")) chkaddrole.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("RoleView")) chkviewrole.Checked = item.IsAllowed;
+
+                //Meter Box
+                if (item.RoleFeatureName.Equals("MeterBoxEditOrDelete")) chkeditdeletemeterbox.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("MeterBoxAdd")) chkaddmeterbox.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("MeterBoxView")) chkviewmeterbox.Checked = item.IsAllowed;
+
+                //village 
+                if (item.RoleFeatureName.Equals("QuarterEditOrDelete")) chkeditdeletevillage.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("QuarterAdd")) chkaddvillage.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("QuarterView")) chkviewvillage.Checked = item.IsAllowed;
+                //Punishment
+                if (item.RoleFeatureName.Equals("PunishmentAdd")) chkviewPunishmentRule.Checked = item.IsAllowed;
+                //bill code 7 Layer
+                //Bill Code  Layer
+                if (item.RoleFeatureName.Equals("BillCodeLayerEditOrDelete")) chkeditdeletebillcodelayer.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("BillCodeLayerAdd")) chkaddbillcodelayer.Checked = item.IsAllowed;
+                if (item.RoleFeatureName.Equals("BillCodeLayerView")) chkviewbillcodelayerlist.Checked = item.IsAllowed;
                 }
+            }
+
+        private void resetCheckControl(bool v) {
+            chkviewcustomerlist.Checked = chkaddcustomer.Checked = chkeditdeletecustomer.Checked = v;
+            chkviewbillpayment.Checked = chkeditdeletebillpayment.Checked = v;
+            chkbillingprocess.Checked = chkviewbillinginvoice.Checked = chkeditbillingprocessdata.Checked = v;
+            chkViewBillUnit.Checked = chkcollectbillunit.Checked = v;
+            chkeditdeletemeter.Checked = chkaddmeter.Checked = chkviewmeterlist.Checked = v;
+            chkeditdeleterole.Checked = chkaddrole.Checked = chkviewrole.Checked = v;
+            chkeditdeletemeterbox.Checked = chkaddmeterbox.Checked = chkviewmeterbox.Checked = v;
+            chkeditdeletevillage.Checked = chkaddvillage.Checked = chkviewvillage.Checked = v;
+            chkviewPunishmentRule.Checked = chkeditdeletebillcodelayer.Checked = chkaddbillcodelayer.Checked = chkviewbillcodelayerlist.Checked = v;
             }
         }
     }
