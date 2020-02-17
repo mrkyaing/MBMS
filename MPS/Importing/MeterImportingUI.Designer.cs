a255e2cc9f749cf31d6eb7c3f04226bde26b8de3
Mg Kyaing <akog02@gmaill.com>
2020-02-17 18:03:04 +0630

    progress bar

--- MPS/Importing/MeterImportingUI.Designer.cs
+++ MPS/Importing/MeterImportingUI.Designer.cs
@@ -23,25 +23,31 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MeterImportingUI));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnSelect = new System.Windows.Forms.Button();
             this.gvCustomer = new System.Windows.Forms.DataGridView();
             this.ofdSelect = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gvCustomer)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.progressBar1);
             this.groupBox1.Controls.Add(this.btnSave);
             this.groupBox1.Controls.Add(this.btnSelect);
             this.groupBox1.Location = new System.Drawing.Point(60, 17);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(419, 122);
+            this.groupBox1.Size = new System.Drawing.Size(419, 144);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Importing Meter Information:";
@@ -71,7 +77,7 @@
             // gvCustomer
             // 
             this.gvCustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvCustomer.Location = new System.Drawing.Point(13, 149);
+            this.gvCustomer.Location = new System.Drawing.Point(13, 171);
             this.gvCustomer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gvCustomer.Name = "gvCustomer";
             this.gvCustomer.Size = new System.Drawing.Size(1072, 260);
@@ -82,11 +88,32 @@
             this.ofdSelect.FileName = "openFileDialog1";
             this.ofdSelect.FileOk += new System.ComponentModel.CancelEventHandler(this.ofdSelect_FileOk);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(53, 113);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(249, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Status:";
+            // 
             // MeterImportingUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1093, 422);
+            this.ClientSize = new System.Drawing.Size(1093, 435);
             this.Controls.Add(this.gvCustomer);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Myanmar3", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -95,6 +122,7 @@
             this.Name = "MeterImportingUI";
             this.Text = "Meter Importing";
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gvCustomer)).EndInit();
             this.ResumeLayout(false);
 
@@ -106,5 +134,8 @@
         private System.Windows.Forms.Button btnSelect;
         private System.Windows.Forms.DataGridView gvCustomer;
         private System.Windows.Forms.OpenFileDialog ofdSelect;
-        }
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
     }
--- MPS/Importing/MeterImportingUI.cs
+++ MPS/Importing/MeterImportingUI.cs
@@ -12,12 +12,14 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MPS.Importing {
     public partial class MeterImportingUI : Form {
         IMeter meterservices;
+        List<Meter> meterList = new List<Meter>();
         DataTable dt = new DataTable();
         public string UserID { get; set; }
         private string Excel03ConString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='Excel 8.0;HDR={1}'";
@@ -33,46 +35,21 @@ namespace MPS.Importing {
 
         private void btnSave_Click(object sender, EventArgs e) {
             if (dt.Rows.Count > 0) {
+               
                 DialogResult ok = MessageBox.Show("are you sure to save data?", "information", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                 if (ok == DialogResult.Yes) {
-                    List<Meter> meterList = new List<Meter>();
+                   
                     foreach (DataRow row in dt.Rows) {
                         bool isdataexit = meterservices.getMeterByMeterNo(row["MeterNo"].ToString());
                         if (isdataexit) {
                             MessageBox.Show("Meter  data already exists in the system for>" + row["MeterNo"].ToString(), "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                             return;
                         }
+                       
                         Meter meter = new Meter();
-                        meter.MeterNo = row["MeterNo"].ToString();
-                        meter.MeterID = Guid.NewGuid().ToString();
-                        meter.Model = row["Model"].ToString();
-                        meter.InstalledDate = Convert.ToDateTime(row["InstalledDate"].ToString());
-                        meter.Phrase = row["Phrase"].ToString();
-                        meter.Wire = row["Wire"].ToString();
-                        meter.BasicCurrent = row["BasicCurrent"].ToString();
-                        meter.iMax = Convert.ToInt32(row["iMax"]);
-                        meter.Voltage = Convert.ToInt32(row["Voltage"]);
-                        meter.ManufactureBy = row["ManufactureBy"].ToString();
-                        meter.Status = row["Status"].ToString();
-                        meter.AvailableYear = Convert.ToInt32(row["AvailableYear"]);
-                        MeterBox meterbox = meterservices.getMeterBoxByMeterBoxNo(row["MeterBoxCode"].ToString());
-                        if (meterbox == null) {
-                            MessageBox.Show("Please define Meter Box Code for>" + meter.MeterNo, "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                            return;
-                        }
-                        meter.MeterBoxID = meterbox.MeterBoxID;
-
-                        meter.MeterBoxSequence = Convert.ToString(row["MeterBoxSequence"]);
-                        MeterType metertype = meterservices.getMeterTypeByMeterTypeCode(row["MeterTypeCode"].ToString());
-                        if (metertype == null) {
-                            MessageBox.Show("Please define Meter Type Code for>" + meter.MeterNo, "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                            return;
-                        }
-                        meter.MeterTypeID = metertype.MeterTypeID;
-                        meter.Active = true;
-                        meter.CreatedDate = DateTime.Now;
-                        meter.CreatedUserID = UserID;
-                        meterList.Add(meter);
+                        progressBar1.Visible = true;
+                        timer1.Enabled = true;
+                        BindRecrods(meter,row);                       
                     }
                     if (meterList.Count > 0) {
                         try {
@@ -86,6 +63,39 @@ namespace MPS.Importing {
                 }//end of Yes Dialog
             }//end of dt.rows.Count>0
             }
+
+        private void BindRecrods(Meter meter, DataRow row) {
+            meter.MeterNo = row["MeterNo"].ToString();
+            meter.MeterID = Guid.NewGuid().ToString();
+            meter.Model = row["Model"].ToString();
+            meter.InstalledDate = Convert.ToDateTime(row["InstalledDate"].ToString());
+            meter.Phrase = row["Phrase"].ToString();
+            meter.Wire = row["Wire"].ToString();
+            meter.BasicCurrent = row["BasicCurrent"].ToString();
+            meter.iMax = Convert.ToInt32(row["iMax"]);
+            meter.Voltage = Convert.ToInt32(row["Voltage"]);
+            meter.ManufactureBy = row["ManufactureBy"].ToString();
+            meter.Status = row["Status"].ToString();
+            meter.AvailableYear = Convert.ToInt32(row["AvailableYear"]);
+            MeterBox meterbox = meterservices.getMeterBoxByMeterBoxNo(row["MeterBoxCode"].ToString());
+            if (meterbox == null) {
+                MessageBox.Show("Please define Meter Box Code for>" + meter.MeterNo, "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            meter.MeterBoxID = meterbox.MeterBoxID;
+
+            meter.MeterBoxSequence = Convert.ToString(row["MeterBoxSequence"]);
+            MeterType metertype = meterservices.getMeterTypeByMeterTypeCode(row["MeterTypeCode"].ToString());
+            if (metertype == null) {
+                MessageBox.Show("Please define Meter Type Code for>" + meter.MeterNo, "Information", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            meter.MeterTypeID = metertype.MeterTypeID;
+            meter.Active = true;
+            meter.CreatedDate = DateTime.Now;
+            meter.CreatedUserID = UserID;
+            meterList.Add(meter);
+        }
 
         private void ofdSelect_FileOk(object sender, CancelEventArgs e) {
             string filePath = ofdSelect.FileName;
@@ -126,5 +136,16 @@ namespace MPS.Importing {
                     }
                 }
             }
+        private void timer1_Tick(object sender, EventArgs e) {
+            progressBar1.PerformStep();
+            for(int i = 1; i <= 100; i++) {
+                // Wait 50 milliseconds.  
+                Thread.Sleep(50);
+                progressBar1.Value = i;            
+            }
+            if (Convert.ToInt32(progressBar1.Value) == 100) {
+                timer1.Enabled = false;
+            }
         }
+    }
     }
